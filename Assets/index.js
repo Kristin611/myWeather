2cc3e5ad453da14729382b6004799ca9aa6598a6
Kris10 <kmrichie611@gmail.com>
2023-09-28 15:58:07 -0500

    added searchCities() to JS file

--- Assets/index.js
+++ Assets/index.js
@@ -1,15 +1,69 @@
 const APIkey = '0db0ce786ae41bdff606b62d15ada316';
-//curl https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=0db0ce786ae41bdff606b62d15ada316
 
-const searchBox = document.getElementById('search'); //grabbing search input box--need to create a function for this like searchCities() to fetch data
-//for search button should I do an on-click=searchCities(), or .addEventListener?
+//Search for a city's weather:
+const spanEl = document.getElementById('emoji')
+const ccHumid = document.getElementById('cc-humid')
+const ccWind = document.getElementById('cc-wind')
+const ccTemp = document.getElementById('cc-temp')
+const currentCityDisplay = document.getElementById('current-city');
+const searchBtn = document.getElementById('searchBtn')
+const searchBox = document.getElementById('search'); 
 
-const currentCityDisplay = document.getElementById('current-cty'); //grabbing <h3>Current City</h3> to populate fetch data depending on which city has been searched. Need to create a separate function for this like currentCityDisplay()?
-const ccTemp = document.getElementById('cc-temp') //grabbing <p> to populate fetch data for current city's temp.
-const ccWind = document.getElementById('cc-wind') //grabbing <p> to populate fetch data for current city's wind.
-const ccHumid = document.getElementById('cc-humid') //grabbing <p> to populate fetch data for current city's humidity.
-//will the variables above go inside the currentCityDisplay()? 
+async function searchCities(city) {
+    
+    fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${APIkey}`
+    ) .then(function(res){
+        return res.json()
+        
+    }) .then(function(weatherData){
+        console.log(weatherData.list[0])
+        currentCityDisplay.textContent = weatherData.city.name
+        ccTemp.textContent = 'Temp: ' + weatherData.list[0].main.temp
+        ccHumid.textContent = 'Humidity: ' + weatherData.list[0].main.humidity
+        ccWind.textContent = 'Wind Speed: ' + weatherData.list[0].wind.speed
+        const icon = weatherData.list[0].weather[0].icon
+        const iconLink = `https://openweathermap.org/img/wn/${icon}.png`
+        const iconImg = `<img src='${iconLink}' alt='weather image'>`
+        spanEl.innerHTML = iconImg
 
-//5-Day Forecast 
 
-//maybe don't use day.js to populate data--can retreive date from fetch data
+        let fiveDayArray = weatherData.list.filter(day => day.dt_txt.includes('12:00:00'))
+        let fiveDaySection = document.querySelector('.five-day-section')
+        let fiveDayCards = ''
+        for (let i = 0; i < fiveDayArray.length; i++) {
+            let cardDate = new Date(fiveDayArray[i].dt_txt).toLocaleString().split(',')[0]
+            let cardIcon = fiveDayArray[i].weather[0].icon
+            let cardLink = `https://openweathermap.org/img/wn/${cardIcon}.png`
+            fiveDayCards += `
+           
+                        <div class="col-sm-6 mb-3 mb-sm-0">
+                          <div class="card">
+                            <div class="card-body">
+                              <h5 class="card-title" id="day-1">${cardDate}</h5>
+                                <div id="emoji-1"><img src='${cardLink}' alt='weather image'></div>
+                                <p id="temp-d1">Temp: ${fiveDayArray[i].main.temp}</p>
+                                <p id="wind-d1">Wind: ${fiveDayArray[i].wind.speed}f</p>
+                                <p id="humid-d1">Humidity: ${fiveDayArray[i].main.humidity}</p>
+                            </div>
+                          </div>
+                        </div>
+            `;
+            fiveDaySection.innerHTML = fiveDayCards
+        }
+    })
+
+   
+    
+}
+
+//5-Day Forecast Display:
+
+
+//create a function for each day in 5-day forecast, like displayDay1(), or create some kind of loop like if it is day-1, display this data?
+
+searchBtn.addEventListener('click', function(event){
+    event.preventDefault()
+    let city = searchBox.value.trim()
+    searchCities(city)
+})
